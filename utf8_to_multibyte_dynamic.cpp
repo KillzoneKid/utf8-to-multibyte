d2a758d175984cc01b2540e9e7969c9c5363ac59
KillzoneKid <killzone_kid@hotmail.co.uk>
2018-01-11 17:05:29 +0000

    unique_ptr, win api handles

--- utf8_to_multibyte_dynamic.cpp
+++ utf8_to_multibyte_dynamic.cpp
@@ -1,6 +1,7 @@
 #include <windows.h> // console functions
 #include <cstdio> // std::printf, std::size_t
 #include <string> // std::string, std::wstring
+#include <memory> // std::unique_ptr, std::make_unique
 
 #define DEFAULT_INPUT_LENGTH 255 // any length > 0 really
 
@@ -9,9 +10,9 @@ int main()
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 
-	wchar_t w_str[DEFAULT_INPUT_LENGTH];
-	unsigned long n_read;
-	void *console = GetStdHandle(STD_INPUT_HANDLE);
+	WCHAR w_str[DEFAULT_INPUT_LENGTH];
+	DWORD n_read;
+	HANDLE console = GetStdHandle(STD_INPUT_HANDLE);
 	std::wstring ws;
 
 	while (ReadConsoleW(console, w_str, DEFAULT_INPUT_LENGTH, &n_read, NULL))
@@ -22,14 +23,13 @@ int main()
 	}
 
 	int mb_size = WideCharToMultiByte(CP_UTF8, 0, ws.data(), ws.length(), NULL, 0, NULL, NULL);
-	char *mb_str = new char[mb_size];
-	
-	WideCharToMultiByte(CP_UTF8, 0, ws.data(), ws.length(), mb_str, mb_size, NULL, NULL);
-	std::string s(mb_str, static_cast<std::size_t>(mb_size));
-	
-	delete[] mb_str;
-	
+	std::unique_ptr<CHAR[]> mb_str = std::make_unique<CHAR[]>(static_cast<std::size_t>(mb_size));
+
+	WideCharToMultiByte(CP_UTF8, 0, ws.data(), ws.length(), mb_str.get(), mb_size, NULL, NULL);
+	std::string s(mb_str.get(), static_cast<std::size_t>(mb_size));
+
+
 	std::printf("ENTERED: %s\n", s.c_str()); // because std::cout prints gibberish
-	
+
 	return 0;
 }
